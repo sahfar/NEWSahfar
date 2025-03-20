15682f238e74d27811fbc068a30714a6d3647f35
sahfar <sahfarclass@gmail.com>
2025-03-20 19:47:50 +0530

    new file

---
+++ project/js/index.js
@@ -0,0 +1,16 @@
+// javascript datatype 
+
+// variable creating using var keyword
+var a=10;
+var a=20;
+console.log(a);
+
+// variable creating using let keyword
+let b=30;
+b=40
+console.log(a+b);
+
+//variable creating using const keyword
+
+const l=90;
+console.log(l);
